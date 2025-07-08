a9b8cf445be5712236f8c11df35808786fbebf9c
Utku Sert <utkusert.arc@gmail.com>
2025-07-08 12:34:06 +0300

    2

--- js/task-3.js
+++ js/task-3.js
@@ -1,18 +1,23 @@
-function filterArray(numbers, value) {
-  const newArray = [];
+const profile = {
+  username: "Jacob",
+  playTime: 300,
 
-  for (const number of numbers) {
-    if (number > value) {
-      newArray.push(number);
-    }
-  }
+  changeUsername(newName) {
+    this.username = newName;
+  },
 
-  return newArray;
-}
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
 
-// Testler
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+};
+
+// Test çağrıları
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
